3e3b23e41b225b52dd61b2d3afeec81257a4a709
Abhishek <bondkiller07@gmail.com>
2026-04-08 18:48:53 +0530

    Updated README and project changes

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,11 +6,12 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://1964c0b74e587189c8fbe41627de0a79@o4506876178464768.ingest.us.sentry.io/4509366354247680",
+  dsn: "https://4d0099d281cb243347915ab7a01b7820@o4511184505602048.ingest.de.sentry.io/4511184548200528",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
